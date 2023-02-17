9b07aa8cc2df288ef16a39a7c2421983bb5ee50d
TobeFX <nwokepraise11@gmail.com>
2023-02-16 17:08:53 -0800

    hello put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,10 @@
 
 /**
  * main - this is the main function
- * 
  * Return: - always 0
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
+}
